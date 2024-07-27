98416820d74ce44d308a1911b5de00d8b6080b23
viswanathr73 <viswanathr73@gmail.com>
2024-07-27 22:35:56 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import ParticlesComponent from './components/particles';
+
+function App() {
+  return (
+    <div className="App">
+      <ParticlesComponent id="particles" />
+      <h1>Particles are so fun!</h1>
+    </div>
+  );
+}
+
+export default App;
